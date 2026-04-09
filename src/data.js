fc18f21afe3075fa157537322e6e5730d38adf52
Moshe Hayoun <yelotag@gmail.com>
2026-04-09 10:03:26 +0300

    v1.0.0.3

--- src/data.js
+++ src/data.js
@@ -1,15 +1,30 @@
 // src/data.js
 
 export const initialSalads = [
-  { id: 1, name: "חומוס בייתי", image: "/image_1.jpg", description: "חומוס עשיר עם שמן זית" },
-  { id: 2, name: "טחינה הר ברכה", image: "/image_72731f.jpg", description: "טחינה אסלית משובחת" },
-  { id: 3, name: "מטבוחה מרוקאית", image: "/image_7275e8.jpg", description: "מבושלת שעות ארוכות" },
-  { id: 4, name: "סלט ביצים", image: "/image_72763f.jpg", description: "קלאסי ונימוח" },
-  { id: 5, name: "חציל בטחינה", image: "/image_72767f.jpg", description: "חציל קלוי על האש" },
-  { id: 6, name: "סלט כרוב לבן", image: "/image_7276bb.jpg", description: "פריך ורענן" },
-  // Add 2 more placeholder salads to reach the required 8 options
-  { id: 7, name: "גזר מרוקאי", image: "https://placehold.co/100x100?text=סלט גזר", description: "פיקנטי וארומטי" },
-  { id: 8, name: "סלט קולסלאו", image: "https://placehold.co/100x100?text=קולסלאו", description: "כרוב וגזר במיונז" },
+  { id: 1, name: "חומוס בייתי", image: "/image_1.jpg", translationKey: "salad_hummus", description: "חומוס עשיר עם שמן זית" },
+  { id: 2, name: "טחינה הר ברכה", image: "/image_72731f.jpg", translationKey: "salad_tahini", description: "טחינה אסלית משובחת" },
+  { id: 3, name: "מטבוחה מרוקאית", image: "/image_7275e8.jpg", translationKey: "salad_matbucha", description: "מבושלת שעות ארוכות" },
+  { id: 4, name: "סלט ביצים", image: "/image_72763f.jpg", translationKey: "salad_eggs", description: "קלאסי ונימוח" },
+  { id: 5, name: "חציל בטחינה", image: "/image_72767f.jpg", translationKey: "salad_eggplant_tahini", description: "חציל קלוי על האש" },
+  { id: 6, name: "סלט כרוב לבן", image: "/image_7276bb.jpg", translationKey: "salad_white_cabbage", description: "פריך ורענן" },
+  { id: 7, name: "חציל יוני", image: "/image_eggplant_yoni.jpg", translationKey: "salad_eggplant_yoni", description: "חציל מטוגן בתיבול ייחודי" },
+  { id: 8, name: "סחוג בשמן זית", image: "/image_schug.jpg", translationKey: "salad_schug", description: "חריף וטעים מאוד" },
+  { id: 9, name: "חציל פיקנטי", image: "/image_spicy_eggplant.jpg", translationKey: "salad_spicy_eggplant", description: "קוביות חציל ברוטב עגבניות" },
+  { id: 10, name: "סלט ירקות חי", image: "/image_vegetable_salad.jpg", translationKey: "salad_fresh_veggies", description: "חסה, עגבניות ומלפפון" },
+  { id: 11, name: "גזר מרוקאי", image: "/image_moroccan_carrot.jpg", translationKey: "salad_moroccan_carrot", description: "פיקנטי וארומטי" },
+  { id: 12, name: "סלט קולסלאו", image: "/image_coleslaw.jpg", translationKey: "salad_coleslaw", description: "כרוב וגזר במיונז" },
+  { id: 13, name: "חציל במיונז", image: "/image_eggplant_mayo.jpg", translationKey: "salad_eggplant_mayo", description: "קלאסי ואהוב" },
+  { id: 14, name: "סלט תפו\"א", image: "/image_potato_salad.jpg", translationKey: "salad_potato", description: "בסגנון ביתי עם מיונז" },
+  { id: 15, name: "סלט סלק אדום", image: "/image_beet_salad.jpg", translationKey: "salad_beet", description: "מתקתק ומרענן" },
+  { id: 16, name: "פלפלים קלויים", image: "/image_roasted_peppers.jpg", translationKey: "salad_roasted_peppers", description: "בתחמיץ ושום" },
+  { id: 17, name: "סלט תירס", image: "/image_corn_salad.jpg", translationKey: "salad_corn", description: "עם גמבה ופטרזיליה" },
+  { id: 18, name: "חציל בטעם כבד", image: "/image_eggplant_liver.jpg", translationKey: "salad_eggplant_liver", description: "מעדן אמיתי" },
+  { id: 19, name: "כרוב סגול", image: "/image_purple_cabbage.jpg", translationKey: "salad_purple_cabbage", description: "במיונז או בתחמיץ" },
+  { id: 20, name: "סלט וולדורף", image: "/image_waldorf.jpg", translationKey: "salad_waldorf", description: "תפוחים, סלרי ואגוזים" },
+  { id: 21, name: "זיתים מבושלים", image: "/image_cooked_olives.jpg", translationKey: "salad_olives", description: "בסגנון מרוקאי פיקנטי" },
+  { id: 22, name: "סלט פסטה", image: "/image_pasta_salad.jpg", translationKey: "salad_pasta", description: "עם ירקות קלויים" },
+  { id: 23, name: "חמוצי הבית", image: "/image_pickles.jpg", translationKey: "salad_pickles", description: "ירקות מוחמצים במקום" },
+  { id: 24, name: "סלט קינואה", image: "/image_quinoa.jpg", translationKey: "salad_quinoa", description: "עם עשבי תיבול וחמוציות" },
 ];
 
 export const mainDishes = [
